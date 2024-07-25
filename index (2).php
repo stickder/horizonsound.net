81361a847df0fa34f3eff03f788f414d2a4a8054
stickder <149419447+stickder@users.noreply.github.com>
2024-07-25 13:30:57 -0500

    Add files via upload

---
+++ index (2).php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página PHP</title>
+</head>
+<body>
+    <h1>¡Hola, mundo!</h1>
+    <p>Esta es mi primera página PHP.</p>
+    <?php
+        echo "<p>Hoy es " . date("Y/m/d") . "</p>";
+    ?>
+</body>
+</html>
